312f25d9b400740f5a9e1a6ffb093ab470db111a
Biruh Tesfaye <biruhtesfaye2121@gmail.com>
2024-04-26 23:57:53 +0300

    updated the jobs spider

--- ethio_jobs/ethio_jobs/spiders/jobs.py
+++ ethio_jobs/ethio_jobs/spiders/jobs.py
@@ -1,4 +1,3 @@
-from typing import Iterable
 import scrapy
 
 
@@ -18,24 +17,87 @@ class JobsSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse, headers=self.headers)
 
     def parse(self, response):
-        res = response.xpath('//tbody[@class="searchResultsJobs"]/tr')
+        """
+        this method is used to parse the job posts lists information
+        it extracts the information from the job posts including the link to
+        the job details page and send it to the parse_detail method to parse the details
+        """
 
-        for r in res:
-            position = r.xpath(
-                ".//td/div/div[@class='listing-title']/h2/a/text()"
+        jobs = response.xpath('//div[@class="listing-section"]')
+
+        for job in jobs[:4]:
+            position = job.xpath(".//div[@class='listing-title']/h2/a/text()").get()
+            recruiter = job.xpath(
+                ".//div[contains(@class, 'listing-info')]/div[contains(@class, 'brief_view')]/div[1]/a/text()"
             ).get()
-            recruiter = r.xpath(
-                ".//td/div/div[contains(@class, 'listing-info')]/div[contains(@class, 'brief_view')]/div[1]/a/text()"
-            ).get()
-
-            yield {"Position": position, "Company": recruiter}
-
-            next_page = response.urljoin(
-                res.xpath(
-                    "//ul[@class='pagination pagination-blue']/li[7]/a/@href"
-                ).get()
+            work_place = (
+                job.xpath(".//span[contains(@class, 'work-palce')][last()]/text()")
+                .get()
+                .strip()
             )
-            if next_page:
-                yield scrapy.Request(
-                    url=next_page, callback=self.parse, headers=self.headers
-                )
+            deadline = job.xpath(
+                ".//div[@class='detailed_view']/div[@class='pull-left']/span[last()]/text()"
+            ).get()
+            level = job.xpath(
+                ".//div[@class='detailed_view']/div[@class='pull-left']/span[5]/text()"
+            ).get()
+            job_detail_link = job.xpath(".//li[@class='viewDetails']/a/@href").get()
+
+            yield {
+                "Position": position,
+                "Company": recruiter,
+                "Work_place": work_place,
+                "Deadline": deadline,
+                "Level": level,
+                "Job_detail_link": job_detail_link,
+            }
+            yield from response.follow(job_detail_link, self.parse_detail)
+
+        # next_page = response.urljoin(
+        #     response.xpath(
+        #         "//ul[@class='pagination pagination-blue']/li[last()]/a/@href"
+        #     ).get()
+        # )
+        # if next_page:
+        #     yield scrapy.Request(
+        #         url=next_page, callback=self.parse, headers=self.headers
+        #     )
+
+    def parse_detail(self, response):
+        """
+        this method is used to parse the job details
+        """
+
+        catagory = (
+            response.xpath("//div[@id='col-wide']/div[1]/div[2]/text()").get().strip()
+        )
+        employement_type = response.xpath(
+            "//div[@id='col-wide']/div[4]/div[2]/text()"
+        ).get()
+        salary = response.xpath("//div[@id='col-wide']/div[5]/div[2]/text()").get()
+        about_recruiter = response.xpath(
+            "//div[@class='listingInfoContent'][1]/p[2]/text()"
+        ).get()
+        job_grade = (
+            response.xpath(
+                "substring-after(//div[@class='listingInfoContent'][1]/p[4]/text(), 'Job grade')"
+            )
+            .get()
+            .strip()
+        )
+        # required_num =
+        # department = ''
+
+        # job_summary = ''
+        # job_responsibities = ''
+        # education_and_experience = ''
+        # personal_skill = ''
+        # how_to_apply = ''
+
+        yield {
+            "Catagory": catagory,
+            "Employement_type": employement_type,
+            "Salary": salary,
+            "About_recruiter": about_recruiter,
+            "Job_grade": job_grade,
+        }
